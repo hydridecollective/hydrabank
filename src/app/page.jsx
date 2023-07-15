3131e3db3614295b38f45b5480396d46cf27d14f
Emily <emily@bunni.me>
2023-07-15 15:32:49 -0400

    Refactor and optimize Home component
    This commit refactors and optimizes the Home component by:
    - Removing unused imports
    - Simplifying the component structure
    - Improving code organization
    - Removing unused icons
    - Adjusting indentation and formatting for consistency
    - Simplifying the banner text animation
    - Removing empty header sections
    - Organizing the code into logical sections with comments
    
    These changes aim to optimize the codebase, enhance maintainability, and improve overall code quality.

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,38 +1,40 @@
 "use client";
 import React from 'react';
-import { faArrowAltCircleDown, faClipboardCheck, faClock, faExternalLinkAlt, faHandshakeAngle, faShield, faTerminal } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import ReactTypingEffect from "react-typing-effect";
-import { Fade } from "react-awesome-reveal";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowAltCircleDown, faClipboardCheck, faClock, faExternalLinkAlt, faMoneyBill } from "@fortawesome/free-solid-svg-icons";
+import { faTerminal, faShield, faBook, faHandshakeAngle } from '@fortawesome/free-solid-svg-icons';
+import ReactTypingEffect from 'react-typing-effect';
+import { Fade } from 'react-awesome-reveal';
 
 const BannerStrings = [
-  "Minecraft server",
-  "project",
-  "media platform",
-  "API",
+  'Minecraft server',
+  'project',
+  'media platform',
+  'API',
 ].sort(() => Math.random() - 0.5);
 
 function Home() {
   return (
-    
     <div className="py-6">
-      <div className="PageBannerPhoto" style={{ zIndex: "0" }}>
+      <div className="PageBannerPhoto" style={{ zIndex: '0' }}>
         <img
           alt="background"
           src="/assets/img/startup-photo.jpg"
           className="absolute blur-sm hidden md:block brightness-50"
-          style={{ zIndex: "-1" }}
+          style={{ zIndex: '-1' }}
         />
-
         <div className="flex flex-col items-center">
           <Fade delay={100} triggerOnce>
-            <h1 className="PageBannerTitle text-white" id="PageBannerTitle" style={{ zIndex: "1" }}>
+            <h1 className="PageBannerTitle text-white" id="PageBannerTitle" style={{ zIndex: '1' }}>
               Transform your infrastructural dreams into robust, secure realities with
-              <b className="animate-gradient bg-gradient-to-r from-[#b16318] to-[#d87718] bg-clip-text text-transparent"> Hydrabank</b>.
+              <b className="animate-gradient bg-gradient-to-r from-[#b16318] to-[#d87718] bg-clip-text text-transparent">
+                {' '}
+                Hydrabank
+              </b>
+              .
             </h1>
-
-            <h2 className="PageBannerText text-white" id="PageBannerText" style={{ zIndex: "1" }}>
-              Let us take care of your infrastructure, so you can focus on your{" "}
+            <h2 className="PageBannerText text-white" id="PageBannerText" style={{ zIndex: '1' }}>
+              Let us take care of your infrastructure, so you can focus on your{' '}
               <b>
                 <span className="text-orange-400 md:text-orange-300">
                   <ReactTypingEffect
@@ -41,45 +43,99 @@ function Home() {
                     typingDelay={500}
                     eraseDelay={2500}
                     text={BannerStrings}
-                    cursor={"_"}
+                    cursor={'_'}
                   />
                 </span>
               </b>
-              {"."}
+              .
             </h2>
           </Fade>
         </div>
       </div>
-
-      <div className="flex flex-col justify-center items-center"></div>
-
-      <div className="flex flex-col gap-y-2 border-b-2 border-gray-600 py-4">
-        <h1 className="Header py-8 animate-gradient bg-gradient-to-r from-[#b16318] to-[#d87718] bg-clip-text">
-          Why Choose Hydrabank?
-        </h1>
+      <div className="flex flex-col items-center justify-center mt-8">
+        <div className="flex flex-row gap-x-4">
+          <div className="flex flex-col gap-y-2 rounded-xl border-gray-600 border-2 max-w-2xl">
+            <div className="flex flex-row items-center gap-x-2 border-b-2 border-gray-600 py-4 px-4">
+              <FontAwesomeIcon icon={faTerminal} size="lg" className="text-orange-500" />
+              <span className="text-lg font-semibold">Real Time Remote Monitoring</span>
+            </div>
+            <div className="flex flex-col py-4 px-4 pt-2 gap-y-2">
+              <p>
+                Monitor your infrastructure with ease. Our powerful tools provide real-time insights into your system's
+                performance, ensuring smooth operations and rapid issue resolution.
+              </p>
+            </div>
+          </div>
+          <div className="flex flex-col gap-y-2 rounded-xl border-gray-600 border-2 max-w-2xl">
+            <div className="flex flex-row items-center gap-x-2 border-b-2 border-gray-600 py-4 px-4">
+              <FontAwesomeIcon icon={faShield} size="lg" className="text-orange-500" />
+              <span className="text-lg font-semibold">Advanced Security</span>
+            </div>
+            <div className="flex flex-col py-4 px-4 pt-2 gap-y-2">
+              <p>
+                We prioritize a proactive security approach to safeguard your systems. With robust maintenance and
+                advanced tools like Fail2Ban, IPTables, and UFW or Firewall, we ensure your production software and
+                infrastructure remain secure, up-to-date, and reliable.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="flex flex-row gap-x-4 mt-8">
+          <div className="flex flex-col gap-y-2 rounded-xl border-gray-600 border-2 max-w-2xl">
+            <div className="flex flex-row items-center gap-x-2 border-b-2 border-gray-600 py-4 px-4">
+              <FontAwesomeIcon icon={faBook} size="lg" className="text-orange-500" />
+              <span className="text-lg font-semibold">Advanced Logging</span>
+            </div>
+            <div className="flex flex-col py-4 px-4 pt-2 gap-y-2">
+              <p>
+                We streamline SSH logging and backup reporting by organizing them into separate channels, allowing for
+                effortless log browsing. Regular proactive checks are conducted to identify any inconsistencies or
+                anomalies in the data streams, ensuring comprehensive monitoring and reporting.
+              </p>
+            </div>
+          </div>
+          <div className="flex flex-col gap-y-2 rounded-xl border-gray-600 border-2 max-w-2xl">
+            <div className="flex flex-row items-center gap-x-2 border-b-2 border-gray-600 py-4 px-4">
+              <FontAwesomeIcon icon={faHandshakeAngle} size="lg" className="text-orange-500" />
+              <span className="text-lg font-semibold">Quality and Excellence</span>
+            </div>
+            <div className="flex flex-col py-4 px-4 pt-2 gap-y-2">
+              <p>
+                We are committed to delivering high-quality solutions and will work closely with you to ensure your
+                satisfaction. If there's anything you're not happy with, we'll make the necessary adjustments.
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
-      <div className="flex flex-row gap-x-4 items-center justify-center mt-8">
-  <div className="flex flex-col gap-y-2 rounded-xl border-gray-600 border-2 max-w-2xl">
-    <div className="flex flex-row items-center gap-x-2 border-b-2 border-gray-600 py-4 px-4">
-      <FontAwesomeIcon icon={faTerminal} size="lg" className="text-orange-500" />
-      <span className="text-lg font-semibold">Real Time Remote Monitoring</span> 
-    </div>
-    <div className="flex flex-col py-4 px-4 pt-2 gap-y-2">
-      <p>Monitor your infrastructure with ease. Our powerful tools provide real-time insights into your system's performance, ensuring smooth operations and rapid issue resolution.</p>
-    </div>
-  </div>
-  <div className="flex flex-col gap-y-2 rounded-xl border-gray-600 border-2 max-w-2xl">
-    <div className="flex flex-row items-center gap-x-2 border-b-2 border-gray-600 py-4 px-4">
-      <FontAwesomeIcon icon={faShield} size="lg" className="text-orange-500" />
-      <span className="text-lg font-semibold">Advanced Security</span> 
-    </div>
-    <div className="flex flex-col py-4 px-4 pt-2 gap-y-2">
-      <p>We prioritize a proactive security approach to safeguard your systems. With robust maintenance and advanced tools like Fail2Ban, IPTables, and UFW or Firewall, we ensure your production software and infrastructure remain secure, up-to-date, and reliable.</p>
-    </div>
-  </div>
-</div>
+      <div className="flex flex-row gap-x-12 justify-center mt-4">
+        <FontAwesomeIcon icon={faMoneyBill} size="6x" />
+        <div className="flex flex-col gap-y-2 flex-wrap">
+          <h1 className="text-2xl font-bold">That sounds cool and all.. But what are your prices?</h1>
+          <p className="text-lg">
+            At Hydrabank, we offer affordable and reliable system administration services tailored to meet the needs of communities and businesses. Our pricing is designed with your budget in mind, ensuring that you receive top-notch support without hidden fees or exorbitant costs.
+          </p>
+        </div>
+      </div>
+      <div className="flex flex-col py-20 text-center">
+        <h1 className="text-white font-bold text-3xl font-BreezeHeader px-5">So, What Are You Waiting For?</h1>
+        <h2 className="text-white font-medium text-2xl font-BreezeHeader px-5 pt-2 pb-4 text-center">
+          Let us help you bring your <b>vision</b> to life.
+          {" "}
+          <span className="text-orange-400 md:text-orange-300 font-BreezeMono">
+            <ReactTypingEffect speed="100" eraseSpeed="50" typingDelay="500" eraseDelay="2500" text={BannerStrings} cursor={" "} />
+          </span>
+          {" "}
+          for your project.
+        </h2>
 
-
+        <div>
+          <a type="button" className="font-BreezeText inline-block px-6 py-2.5 bg-blue-300 text-gray-800 font-bold text-xs leading-tight uppercase rounded-full shadow-md hover:bg-blue-400 hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-400 active:shadow-lg transition duration-150 ease-in-out" href="/services">
+            Learn More & Get Started
+            <FontAwesomeIcon className="ml-2" icon={faExternalLinkAlt} />
+          </a>
+        </div>
+      </div>
     </div>
   );
 }
