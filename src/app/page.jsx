01f8a17f705146f92bcb98dee2ff01fb00e25046
Emily <emily@bunni.me>
2023-07-20 14:28:44 -0400

    # Optimize and Clean Up Home Component
    ## Description
    This commit optimizes and cleans up the  component in the client-side code. The changes include the following:
    - Extracted a  function to generate repetitive card elements for improved code readability and maintainability.
    - Added proper indentation and line breaks to enhance code formatting.
    
    ## Details
    - The  function now generates the four cards with their respective data (icon, title, and description) to reduce code duplication.
    - The code has been properly indented and formatted to improve code readability.
    
    ## Co-authored-by: Emily <ohemilyy@proton.me>

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,4 +1,5 @@
 "use client";
+import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faExternalLinkAlt, faMoneyBill } from "@fortawesome/free-solid-svg-icons";
 import { faTerminal, faShield, faBook, faHandshakeAngle } from '@fortawesome/free-solid-svg-icons';
@@ -51,56 +52,10 @@ function Home() {
       <div className="flex flex-col">
         <div className="flex min-h-screen">
           <div className="my-auto w-full flex flex-wrap justify-around p-16 px-10 sm:px-16">
-            <div className="col flex flex-col rounded-xl border-gray-600 border-2">
-              <div className="flex flex-row items-center gap-x-2 border-b-2 border-gray-600 p-4">
-                <FontAwesomeIcon icon={faTerminal} size="lg" className="text-orange-500" />
-                <span className="text-lg sm:text-2xl font-semibold">Real Time Remote Monitoring</span>
-              </div>
-              <div className="text-base sm:text-xl p-4 pt-2 gap-y-2">
-                <p>
-                  Monitor your infrastructure with ease. Our powerful tools provide real-time insights into your system&apos;s
-                  performance, ensuring smooth operations and rapid issue resolution.
-                </p>
-              </div>
-            </div>
-            <div className="col flex flex-col rounded-xl border-gray-600 border-2">
-              <div className="flex flex-row items-center gap-x-2 border-b-2 border-gray-600 p-4">
-                <FontAwesomeIcon icon={faShield} size="lg" className="text-orange-500" />
-                <span className="text-lg sm:text-2xl font-semibold">Advanced Security</span>
-              </div>
-              <div className="text-base sm:text-xl p-4 pt-2 gap-y-2">
-                <p>
-                  We prioritize a proactive security approach to safeguard your systems. With robust maintenance and
-                  advanced tools like Fail2Ban, IPTables, and UFW or Firewall, we ensure your production software and
-                  infrastructure remain secure, up-to-date, and reliable.
-                </p>
-              </div>
-            </div>
-            <div className="col flex flex-col rounded-xl border-gray-600 border-2">
-              <div className="flex flex-row items-center gap-x-2 border-b-2 border-gray-600 p-4">
-                <FontAwesomeIcon icon={faBook} size="lg" className="text-orange-500" />
-                <span className="text-lg sm:text-2xl font-semibold">Advanced Logging</span>
-              </div>
-              <div className="text-base sm:text-xl p-4 pt-2 gap-y-2">
-                <p>
-                  We streamline SSH logging and backup reporting by organizing them into separate channels, allowing for
-                  effortless log browsing. Regular proactive checks are conducted to identify any inconsistencies or
-                  anomalies in the data streams, ensuring comprehensive monitoring and reporting.
-                </p>
-              </div>
-            </div>
-            <div className="col flex flex-col rounded-xl border-gray-600 border-2">
-              <div className="flex flex-row items-center gap-x-2 border-b-2 border-gray-600 p-4">
-                <FontAwesomeIcon icon={faHandshakeAngle} size="lg" className="text-orange-500" />
-                <span className="text-lg sm:text-2xl font-semibold">Quality and Excellence</span>
-              </div>
-              <div className="text-base sm:text-xl p-4 pt-2 gap-y-2">
-                <p>
-                  We are committed to delivering high-quality solutions and will work closely with you to ensure your
-                  satisfaction. If there&apos;s anything you&apos;re not happy with, we&apos;ll make the necessary adjustments.
-                </p>
-              </div>
-            </div>
+            {renderCard(faTerminal, "Real Time Remote Monitoring", "Monitor your infrastructure with ease. Our powerful tools provide real-time insights into your system's performance, ensuring smooth operations and rapid issue resolution.")}
+            {renderCard(faShield, "Advanced Security", "We prioritize a proactive security approach to safeguard your systems. With robust maintenance and advanced tools like Fail2Ban, IPTables, and UFW or Firewall, we ensure your production software and infrastructure remain secure, up-to-date, and reliable.")}
+            {renderCard(faBook, "Advanced Logging", "We streamline SSH logging and backup reporting by organizing them into separate channels, allowing for effortless log browsing. Regular proactive checks are conducted to identify any inconsistencies or anomalies in the data streams, ensuring comprehensive monitoring and reporting.")}
+            {renderCard(faHandshakeAngle, "Quality and Excellence", "We are committed to delivering high-quality solutions and will work closely with you to ensure your satisfaction. If there's anything you're not happy with, we'll make the necessary adjustments.")}
           </div>
         </div>
         <div className="px-10 flex min-h-screen flex-col items-center">
@@ -116,17 +71,18 @@ function Home() {
           <div className="mb-auto flex flex-col pt-20 text-center">
             <h1 className="text-white font-bold text-3xl font-header px-5">So, What Are You Waiting For?</h1>
             <h2 className="text-white font-medium text-2xl font-header px-5 pt-2 pb-4 text-center">
-              Let us help you bring your <b>vision</b> to life.
-              {" "}
+              Let us help you bring your <b>vision</b> to life.{' '}
               <span className="text-orange-400 md:text-orange-300 font-spacemono">
                 <ReactTypingEffect speed="100" eraseSpeed="50" typingDelay="500" eraseDelay="2500" text={BannerStrings} cursor={" "} />
               </span>
-              {" "}
-              for your project.
+              {' '}for your project.
             </h2>
-
             <div>
-              <a type="button" className="font-golos inline-block px-6 py-2.5 bg-blue-300 text-gray-800 font-bold text-xs leading-tight uppercase rounded-full shadow-md hover:bg-blue-400 hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-400 active:shadow-lg transition duration-150 ease-in-out" href="/services">
+              <a
+                type="button"
+                className="font-golos inline-block px-6 py-2.5 bg-blue-300 text-gray-800 font-bold text-xs leading-tight uppercase rounded-full shadow-md hover:bg-blue-400 hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-400 active:shadow-lg transition duration-150 ease-in-out"
+                href="/services"
+              >
                 Learn More & Get Started
                 <FontAwesomeIcon className="ml-2" icon={faExternalLinkAlt} />
               </a>
@@ -135,6 +91,20 @@ function Home() {
         </div>
       </div>
     </>
+  );
+}
+
+function renderCard(icon, title, description) {
+  return (
+    <div className="col flex flex-col rounded-xl border-gray-600 border-2">
+      <div className="flex flex-row items-center gap-x-2 border-b-2 border-gray-600 p-4">
+        <FontAwesomeIcon icon={icon} size="lg" className="text-orange-500" />
+        <span className="text-lg sm:text-2xl font-semibold">{title}</span>
+      </div>
+      <div className="text-base sm:text-xl p-4 pt-2 gap-y-2">
+        <p>{description}</p>
+      </div>
+    </div>
   );
 }
 
